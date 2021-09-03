dee487eebb350848943b03c1498754bd36e500de
adamhungerford <adhungerford02@gmail.com>
2021-09-02 23:44:01 -0400

    Incorporated basic functionality.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var Engine = Matter.Engine,
     Mouse = Matter.Mouse,
     Composite = Matter.Composite,
     Bodies = Matter.Bodies;
+    Body = Matter.Body;
 
 // create engine
 var engine = Engine.create(),
@@ -18,7 +19,7 @@ var render = Render.create({
     options: {
         width: 1000,
         height: 600,
-        showAngleIndicator: true
+        showAngleIndicator: false
     }
 });
 
@@ -28,15 +29,32 @@ Render.run(render);
 var runner = Runner.create();
 Runner.run(runner, engine);
 
-// add bodies
-var stack = Composites.stack(300, 600 - 21 - 20 * 20, 10, 10, 20, 0, function(x, y) {
-    return Bodies.rectangle(x, y, 40, 40);
-});
+percy = function(xx, yy){
+        x = xx,
+        y = yy,
+        skull = Bodies.circle(x, y, 30),
+        teeth = Bodies.rectangle(x, y-30, 40, 40, { render: skull.render });
+
+    var percy = Body.create({
+        parts: [skull, teeth]
+    });
+
+return percy;
+};
+
+drill = function(xx, yy){
+    x = xx,
+    y = yy,
+    newdrill = Bodies.polygon(x,y,3,50);
+
+return newdrill;
+}
 
 Composite.add(world, [
-    // walls
-    Bodies.rectangle(600, 600, 800, 50, { isStatic: true }),    //ceiling
-    stack
+    Bodies.rectangle(600, 600, 600, 50, { isStatic: true }),    //floor
+    Bodies.rectangle(0, 600, 300, 50, {isStatic: true }),
+    Bodies.rectangle(-100, 500, 50, 300, {isStatic: true }),
+    Bodies.rectangle(125, 500, 50, 300, {isStatic: true }),
 ]);
 
 // add mouse control
@@ -61,3 +79,60 @@ Render.lookAt(render, {
     min: { x: 0, y: 0 },
     max: { x: 800, y: 600 }
 });
+
+document.getElementById("spawn").onclick = function() {
+    newpercy = percy(0,0);
+    newdrill = drill(0,0);
+    const blocks = [newpercy, newdrill];
+    choose = Math.floor(Math.random() * 2);
+    console.log(choose);
+    Composite.add(world, [blocks[choose]]);
+};
+
+document.getElementById("reset").onclick = function(){
+    Composite.clear(world);
+    Engine.clear(engine);
+    Render.stop(render);
+    Runner.stop(runner);
+    render.canvas.remove();
+    render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 1000,
+            height: 600,
+            showAngleIndicator: false
+        }
+    });
+    Render.run(render)
+    Runner.run(runner, engine);
+    Composite.add(world, [
+        Bodies.rectangle(600, 600, 600, 50, { isStatic: true }),    //floor
+        Bodies.rectangle(0, 600, 300, 50, {isStatic: true }),
+        Bodies.rectangle(-100, 500, 50, 300, {isStatic: true }),
+        Bodies.rectangle(125, 500, 50, 300, {isStatic: true }),
+    ]);
+
+    var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+    Composite.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering   
+    render.mouse = mouse;
+
+// fit the render viewport to the scene
+    Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: 800, y: 600 }
+    });
+
+}
